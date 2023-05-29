0979ef652943c7682634821b1c8c23a0e966eadc
irikkenji <93337469+irikkenji@users.noreply.github.com>
2023-05-29 17:42:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,17 @@ class Usuario(BaseModel):
 # Simulação de armazenamento de dados
 db: Dict[int, Usuario] = {}
 
+# Modelo de dados
+class Produtos(BaseModel):
+    prod_id: int
+    prod_nome: str
+    prod_descr: str
+    prod_preco: float
+
+# Simulação de armazenamento de dados
+db: Dict[int, Produtos] = {}
+
+    
 # Operação Create
 @app.post("/usuarios/")
 async def create_usuario(usuario: Usuario):
@@ -45,5 +56,35 @@ async def update_usuario(usuario_log_id: int, usuario: Usuario):
 async def delete_usuario(usuario_log_id: int):
     if usuario_log_id in db:
         del db[usuario_log_id]
+        return {"message": "Usuário excluído"}
+    return {"message": "Usuário não encontrado"}
+
+# Operação Create
+@app.post("/produtos/")
+async def create_produto(produto: Produto):
+    db[produto.log_id] = produto
+    return {"message": "Usuário criado"}
+
+# Operação Read
+@app.get("/produtos/{produto_prod_id}")
+async def read_produto(produto_prod_id: int):
+    produto = db.get(produto_prod_id)
+    if produto:
+        return produto
+    return {"message": "Usuário não encontrado"}
+
+# Operação Update
+@app.put("/produtos/{produto_prod_id}")
+async def update_produto(produto_prod_id: int, produto: Produto):
+    if produto_prod_id in db:
+        db[produto_prod_id] = produto
+        return {"message": "Usuário atualizado"}
+    return {"message": "Usuário não encontrado"}
+
+# Operação Delete
+@app.delete("/produtos/{produto_prod_id}")
+async def delete_produto(produto_prod_id: int):
+    if produto_prod_id in db:
+        del db[produto_prod_id]
         return {"message": "Usuário excluído"}
     return {"message": "Usuário não encontrado"}
